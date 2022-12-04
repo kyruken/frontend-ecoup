957493e8fc592e2b1a1df3cd9d6280a51728d0ee
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-04 02:17:20 -0800

    add login path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import Homepage from './Homepage';
 import Questionpage from './Questionpage';
+import Loginpage from './Loginpage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route exact path='/question/:questionId' element={<Questionpage />} />
+          <Route exact path='/login' element={<Loginpage />} />
+
         </Routes>
       </div>
     </Router>
