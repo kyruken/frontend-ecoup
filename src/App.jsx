59bb73889ab2a64c4bf5a2a9b323974e4de3d7e8
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-04 02:59:27 -0800

    add aboutus and signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import Homepage from './Homepage';
 import Questionpage from './Questionpage';
 import Loginpage from './Loginpage';
+import Signuppage from './Signuppage';
+import Aboutuspage from './Aboutuspage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
@@ -13,7 +15,9 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route exact path='/question/:questionId' element={<Questionpage />} />
+          <Route exact path='/aboutus' element={<Aboutuspage />} />
           <Route exact path='/login' element={<Loginpage />} />
+          <Route exact path='/sign-up' element={<Signuppage />} />
 
         </Routes>
       </div>
