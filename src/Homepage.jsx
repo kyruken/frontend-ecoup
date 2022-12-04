3422f76c97521e56f6ba7fe5248eefcd0f15ade8
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-04 01:16:56 -0800

    paste previous code into homepage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import Question from './components/question';
+import { Link } from 'react-router-dom';
+export default function Homepage() {
+    const [questions, setQuestions] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('http://localhost:3000/questions')
+            .then(res => res.json())
+            .then(data => setQuestions(data.questions))
+    }, [])
+
+    const questionElements = questions.map(question => {
+        return <Link to={`/question/${question._id}`}><Question className="problem"
+            title={question.title}
+            description={question.description}
+            difficulty={question.difficulty}
+            key={question._id}
+        /></Link>
+    })
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="daily">
+                    <p className="probtitle">Problem of the day</p>
+                    <p className="probprompt">thingy thingy thing</p>
+                </div>
+                <div className="problist">
+                    {questionElements}
+                </div>
+            </main>
+        </div>
+
+    )
+}
