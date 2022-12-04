a28b1a6219e1ab6dc46f9fe57425f26fce0e6d92
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-04 00:15:55 -0800

    add fetch, add state, show questionElements

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import './App.css'
-
+import React from 'react';
+import './App.css';
 import Header from './components/header';
+import Question from './components/question';
 function App() {
+  const [questions, setQuestions] = React.useState([]);
 
+  React.useEffect(() => {
+    fetch('http://localhost:3000/questions')
+    .then(res => res.json())
+    .then(data => setQuestions(data.questions))
+  }, [])
+
+  const questionElements = questions.map(question => {
+    return <Question className="problem" 
+      title={question.title}
+      description={question.description}
+      key={question._id}
+    />
+  })
   return (
     <div className="App">
       <Header />
       <main>
         <div className="daily">
           <p className="probtitle">Problem of the day</p>
-          <p className="probprompmt">thingy thingy thing</p>
+          <p className="probprompt">thingy thingy thing</p>
         </div>
 
         <div className="problist">
-          <div className="problem">blank</div>
-          <div className="problem">blank</div>
-          <div className="problem">blank</div>
-          <div className="problem">blank</div>
+          {questionElements}
         </div>
       </main>
     </div>
