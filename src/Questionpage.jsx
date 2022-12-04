a7ec3f53441c4511d3b44dcca58aabb0d52b843c
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-04 01:58:25 -0800

    add submission elements

--- src/Questionpage.jsx
+++ src/Questionpage.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import './App.css';
-
+import Submission from './components/submission'
 import { useParams, Link } from 'react-router-dom';
 
 export default function Questionpage() {
     const questionId = useParams().questionId;
     const [question, setQuestion] = React.useState({});
+    const [submissions, setSubmissions] = React.useState([]);
 
     console.log('rerender');
     React.useEffect(() => {
-        fetch(`http://localhost:3000/questions/${questionId}`)
-        .then(res => res.json())
-        .then(data => setQuestion(data.question));
-
+        Promise.all([
+            fetch(`http://localhost:3000/questions/${questionId}`)
+            .then(res => res.json()),
+            fetch(`http://localhost:3000/questions/${questionId}/submissions`)
+            .then(res => res.json())
+        ]).then(data => {
+            setQuestion(data[0].question);
+            setSubmissions(data[1].submissions);
+        })
     }, [])
+
+    const submissionElements = submissions.map(submission => {
+        return <Submission 
+            answer={submission.answer}
+            key={submission._id}
+        />
+    })
     return (
         <div>
             <Link to='/'>Back</Link>
             <p>{question.difficulty}</p>
             <h2>{question.title}</h2>
             <p>{question.description}</p>
+            <h3>Submissions</h3>
+            {submissionElements}
         </div>
     )
 }
