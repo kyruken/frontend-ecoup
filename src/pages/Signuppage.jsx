8d3e4dc34b989cd2979530b29950500863ecfb00
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-12 10:18:58 -0800

    add logic for creating user

--- src/pages/Signuppage.jsx
+++ src/pages/Signuppage.jsx
@@ -1,20 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Signuppage() {
+    const [registerUsername, setRegisterUsername] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    const [registerEmail, setRegisterEmail] = useState('');
+    const navigate = useNavigate();
+
+
+    const registerUser = (e) => {
+        e.preventDefault();
+        axios({
+            method: "POST",
+            data: {
+                username: registerUsername,
+                password: registerPassword,
+                email: registerEmail
+            },
+            url: "http://localhost:3000/register"
+        }).then(navigate('/login'));
+    }
+
     return (
         <div className='padding-all-2'>
             <Link to='/'>Back</Link>
             <div className="container padding-tb-1">
                 <h1>Sign-up</h1>
-                <form method="post" action="#">
-                    <fieldset>
-                        <input id="username" name="username" type="text" placeholder="Enter Username" required />
-                        <input id="password" name="password" type="password" placeholder="Enter Password" required />
-                        <input id="password" name="password" type="password" placeholder="Re-enter Password" required />
-                        <input id="email" name="email" type="email" placeholder="Enter E-mail Address" required />
-                    </fieldset>
-                    <input type="submit" value="Submit" />
-                </form>
+                <fieldset>
+                    <input id="username" name="username" type="text" placeholder="Enter Username" onChange={(e) => setRegisterUsername(e.target.value)} required />
+                    <input id="password" name="password" type="password" placeholder="Enter Password" onChange={(e) => setRegisterPassword(e.target.value)}  required />
+                    <input id="email" name="email" type="email" placeholder="Enter E-mail Address" onChange={(e) => setRegisterEmail(e.target.value)}  required />
+                </fieldset>
+                <button onClick={(e) => registerUser(e)}>Submit</button> 
             </div>
         </div>
     )
