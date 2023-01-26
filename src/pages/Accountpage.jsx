510dff86c090225f2f6fed8688cda1335bb996dd
Avery Kyle Arenas <kyruken@gmail.com>
2023-01-26 12:27:53 -0800

    fix recent activity fetching, style page

--- src/pages/Accountpage.jsx
+++ src/pages/Accountpage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Navbar from '../components/navbar';
+import '../Profile.css';
 
 export default function Accountpage() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user') || null));
@@ -10,34 +11,42 @@ export default function Accountpage() {
     useEffect(() => {
         for (let x = 0; x < user.questions.length; x++) {
             fetch(`http://localhost:3000/questions/${user.questions[x]}`)
-            .then(res => res.json())
-            .then(data => setRecentActivity(prevState => [...prevState, data.question.description]))
+            .then(res => res.json()) 
+            .then(data => setRecentActivity(prevState => {
+                let array = [...prevState];
+                array.push(data.question.title);
+                return array;
+            }));
         }
         setUserSubmissions(user.submissions.length);
         setUserQuestions(user.questions.length);
     }, [])
-    
-    const recentActivityElements = recentActivity.map(item => <div>{item}</div>)
+
+    console.log(recentActivity);
+    const recentActivityElements = recentActivity.map(item => <div className='activity-container'>{item}</div>)
 
     return (
         <div>
             <Navbar />
-            <div>
-                <h2>{user.username}</h2>
-                <p>{user.email}</p>
+            <div className="profile">
+                <div>
+                    <div className='profile-container'>
+                        <h2>Profile</h2>
+                        <h3>{user.username}</h3>
+                        <p>{user.email}</p>
+                    </div>
+                    <div className='profile-container'>
+                        <h2>Problems</h2>
+                        <h3>Submissions: <span>{userSubmissions}</span></h3>
+                        <h3>Problems solved:<span>{userQuestions}</span></h3>
+                    </div>
+                </div>
 
-            </div>
-
-            <div>
-                <h2>Submissions: <span>{userSubmissions}</span></h2>
-
-                <h2>Problems solved:<span>{userQuestions}</span></h2>
-            </div>
-
-            <div>
+            <div className='recent-activity'>
                 <h2>Recent Activity</h2>
                 {recentActivityElements}
 
+            </div>
             </div>
         </div>
     )
